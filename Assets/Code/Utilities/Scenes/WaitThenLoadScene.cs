f4eb5e290b81c6ac8b7fbeb0bbc98d65f7f7b2f9
joel.page <joel.page@codemasters.com>
2020-07-17 23:33:32 +0100

    Add Front End Flow - Core -> Splash Screen -> Main Menu Add Placeholder Textures Add Placeholder Video

---
+++ Assets/Code/Utilities/Scenes/WaitThenLoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+public class WaitThenLoadScene : MonoBehaviour
+{
+    public float seconds = 0.0f;
+    public Object scene;
+
+    void Awake()
+    {
+        Assert.IsTrue(scene.GetType() != typeof(Scene), "Object used in WaitThenLoadScene is not a Scene!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(seconds);
+        // WARNING - THIS DOES NOT WORK IN BUILDS, ONLY IN THE EDITOR
+        // See : https://answers.unity.com/questions/605184/scene-as-a-variable.html
+        SceneManager.LoadScene(scene.name);
+    }
+}
