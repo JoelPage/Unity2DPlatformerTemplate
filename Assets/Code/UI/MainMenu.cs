970a20a0709ab6dc295bcf04227535f785a51248
joelpage <joelpage92@gmail.com>
2020-08-16 17:01:43 +0100

    Add basic Main and Options Menus

---
+++ Assets/Code/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // 3 is Level 1
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Application.Quit();");
+        Application.Quit();
+    }
+}
